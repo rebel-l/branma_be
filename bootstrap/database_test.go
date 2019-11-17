ceab0c4ed77a9bd66a62583177356043f429ca6a
Lars Gaubisch <l.gaubisch@googlemail.com>
2019-11-18 00:24:48 +0100

    added empty tests to have them in coverage report

---
+++ bootstrap/database_test.go
@@ -0,0 +1,11 @@
+package bootstrap
+
+import "testing"
+
+func TestDatabase(t *testing.T) {
+	t.Skip("TODO") // TODO: implement
+}
+
+func TestDatabaseReset(t *testing.T) {
+	t.Skip("TODO") // TODO: implement
+}
