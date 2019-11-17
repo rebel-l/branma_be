4b1c844da9638af88cce7b088439c41adee51d76
Lars Gaubisch <l.gaubisch@googlemail.com>
2019-11-17 23:41:16 +0100

    improved error messages

--- bootstrap/database.go
+++ bootstrap/database.go
@@ -1,6 +1,7 @@
 package bootstrap
 
 import (
+	"fmt"
 	"path/filepath"
 
 	"github.com/rebel-l/schema"
@@ -18,27 +19,27 @@ const (
 func Database(storagePath, scriptPath, version string) (*sqlx.DB, error) {
 	fileName, err := createStorage(storagePath)
 	if err != nil {
-		return nil, err // TODO: pimp error
+		return nil, fmt.Errorf("bootstrap database, create storage failed: %v", err)
 	}
 
 	db, err := sqlx.Open("sqlite3", fileName)
 	if err != nil {
-		return nil, err // TODO: pimp error
+		return nil, fmt.Errorf("bootstrap database, open database failed: %w", err)
 	}
 
 	err = createSchema(db, scriptPath, version)
 
-	return db, err // TODO: pimp error
+	return db, fmt.Errorf("bootstrap database, create schema failed: %w", err)
 }
 
 func createStorage(path string) (string, error) {
 	if err := osutils.CreateDirectoryIfNotExists(path); err != nil {
-		return "", err // TODO: pimp error
+		return "", err
 	}
 
 	fileName := filepath.Join(path, storageFileName)
 	if err := osutils.CreateFileIfNotExists(fileName); err != nil {
-		return "", err // TODO: pimp error
+		return "", err
 	}
 
 	return fileName, nil
@@ -47,5 +48,6 @@ func createStorage(path string) (string, error) {
 func createSchema(db *sqlx.DB, scriptPath, version string) error {
 	s := schema.New(db)
 	s.WithProgressBar()
-	return s.Upgrade(scriptPath, version) // TODO: check error and revert last
+
+	return s.Upgrade(scriptPath, version)
 }
