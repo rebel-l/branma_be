97c8b7cedf7ce54db33dc1aa9683bfa1d859d9b5
Lars Gaubisch <l.gaubisch@googlemail.com>
2020-01-27 23:46:10 +0100

    covered branch update from store with tests

--- branch/branchstore/branch_test.go
+++ branch/branchstore/branch_test.go
@@ -2,14 +2,15 @@ package branchstore_test
 
 import (
 	"context"
+	"database/sql"
 	"errors"
 	"testing"
-
-	"github.com/rebel-l/branma_be/repository/repositorystore"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 
 	"github.com/rebel-l/branma_be/branch/branchstore"
+	"github.com/rebel-l/branma_be/repository/repositorystore"
 	"github.com/rebel-l/branma_be/test"
 )
 
@@ -139,7 +140,7 @@ func TestBranch_Read(t *testing.T) { // nolint:funlen
 		expectedErr error
 	}{
 		{
-			name:        "repository is nil",
+			name:        "branch is nil",
 			expectedErr: repositorystore.ErrIDMissing,
 		},
 		{
@@ -181,6 +182,163 @@ func TestBranch_Read(t *testing.T) { // nolint:funlen
 			err := testCase.actual.Read(context.Background(), db)
 			test.CheckErrors(t, testCase.expectedErr, err)
 			testBranch(t, testCase.expected, testCase.actual)
+		})
+	}
+}
+
+func TestBranch_Update(t *testing.T) { // nolint:funlen
+	if testing.Short() {
+		t.Skip("long running test")
+	}
+
+	// 1. setup
+	db := test.Setup(t, testCluster, "storeUpdate")
+
+	defer func() {
+		if err := db.Close(); err != nil {
+			t.Fatalf("unable to close database connection: %v", err)
+		}
+	}()
+
+	repo := &repositorystore.Repository{Name: "testrepoupdate", URL: "testrepoupdate.url"}
+	if err := repo.Create(context.Background(), db); err != nil {
+		t.Fatalf("preparing data failed: %v", err)
+	}
+
+	// 2. test
+	testCases := []struct {
+		name        string
+		prepare     *branchstore.Branch
+		actual      *branchstore.Branch
+		expected    *branchstore.Branch
+		expectedErr error
+	}{
+		{
+			name:        "branch is nil",
+			expectedErr: branchstore.ErrDataMissing,
+		},
+		{
+			name: "branch has no name",
+			actual: &branchstore.Branch{
+				ID:             1,
+				TicketID:       "JIRA-1",
+				ParentTicketID: "JIRA-2",
+				RepositoryID:   1,
+				TicketSummary:  "a nice summary",
+				TicketStatus:   "in progress",
+				TicketType:     "improvement",
+				Closed:         true,
+			},
+			expectedErr: branchstore.ErrDataMissing,
+		},
+		{
+			name: "branch has no repository id",
+			actual: &branchstore.Branch{
+				ID:             1,
+				Name:           "myname",
+				TicketID:       "JIRA-1",
+				ParentTicketID: "JIRA-2",
+				TicketSummary:  "a nice summary",
+				TicketStatus:   "in progress",
+				TicketType:     "improvement",
+				Closed:         true,
+			},
+			expectedErr: repositorystore.ErrDataMissing,
+		},
+		{
+			name: "branch has no ID",
+			actual: &branchstore.Branch{
+				Name:           "myname",
+				TicketID:       "JIRA-1",
+				ParentTicketID: "JIRA-2",
+				RepositoryID:   1,
+				TicketSummary:  "a nice summary",
+				TicketStatus:   "in progress",
+				TicketType:     "improvement",
+				Closed:         true,
+			},
+			expectedErr: branchstore.ErrIDMissing,
+		},
+		{
+			name: "success",
+			prepare: &branchstore.Branch{
+				Name:           "myname",
+				TicketID:       "JIRA-1",
+				ParentTicketID: "JIRA-2",
+				RepositoryID:   1,
+				TicketSummary:  "a nice summary",
+				TicketStatus:   "in progress",
+				TicketType:     "improvement",
+				Closed:         true,
+			},
+			actual: &branchstore.Branch{
+				ID:             1,
+				Name:           "new name",
+				TicketID:       "NEW-1",
+				ParentTicketID: "NEW-2",
+				RepositoryID:   1,
+				TicketSummary:  "a new summary",
+				TicketStatus:   "done",
+				TicketType:     "bug",
+				Closed:         false,
+			},
+			expected: &branchstore.Branch{
+				ID:             1,
+				Name:           "new name",
+				TicketID:       "NEW-1",
+				ParentTicketID: "NEW-2",
+				RepositoryID:   1,
+				TicketSummary:  "a new summary",
+				TicketStatus:   "done",
+				TicketType:     "bug",
+				Closed:         false,
+			},
+		},
+		{
+			name: "not existing branch",
+			actual: &branchstore.Branch{
+				ID:             2,
+				Name:           "myname",
+				TicketID:       "JIRA-1",
+				ParentTicketID: "JIRA-2",
+				RepositoryID:   1,
+				TicketSummary:  "a nice summary",
+				TicketStatus:   "in progress",
+				TicketType:     "improvement",
+				Closed:         true,
+			},
+			expectedErr: sql.ErrNoRows,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			if testCase.prepare != nil {
+				_ = testCase.prepare.Create(context.Background(), db)
+				time.Sleep(1 * time.Second)
+			}
+
+			err := testCase.actual.Update(context.Background(), db)
+			test.CheckErrors(t, testCase.expectedErr, err)
+			testBranch(t, testCase.expected, testCase.actual)
+
+			if testCase.prepare != nil && testCase.actual != nil {
+				if testCase.prepare.CreatedAt != testCase.actual.CreatedAt {
+					t.Errorf(
+						"expected created at '%s' but got '%s'",
+						testCase.prepare.CreatedAt.String(),
+						testCase.actual.CreatedAt.String(),
+					)
+				}
+
+				if testCase.prepare.ModifiedAt.After(testCase.actual.ModifiedAt) {
+					t.Errorf(
+						"expected modified at '%s' to be before but got '%s'",
+						testCase.prepare.ModifiedAt.String(),
+						testCase.actual.ModifiedAt.String(),
+					)
+				}
+			}
 		})
 	}
 }
